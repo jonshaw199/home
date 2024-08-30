7703a0953718696d692398f587bf424284b9a932
Jon Shaw <jonshaw199@gmail.com>
2024-08-30 00:38:07 -0700

    fix subscribe, logging

--- services/controller/controller.py
+++ services/controller/controller.py
@@ -14,6 +14,8 @@ logging.basicConfig(
     ]
 )
 
+REQUIRED_KEYS = {"location", "device_type", "device_id"}
+
 class Controller:
     def __init__(self):
         self.websocket_client = WebsocketClient(self.handle_message_ws)
@@ -25,20 +27,25 @@ class Controller:
         if topic:
             self.mqtt_client.publish(topic, message)
         else:
-            logging.error("Cannot publish; invalid topic")
+            # Assuming the message isn't important to the server if no topic
+            logging.error(f"Cannot publish websocket message to mqtt (no topic provided): {message}")
 
     def handle_connect_mqtt(self):
-        self.mqtt_client.subscribe("*")
+        # Subscribe to everything; our job is to keep the server in the loop
+        self.mqtt_client.subscribe("#")
 
     def handle_message_mqtt(self, topic, message):
         logging.info("Handle message: %s", message)
+        # We are subscribed to everything; pass along messages to server
+        # TODO: Do we need to include "topic" as well?
         self.websocket_client.send(message)
 
     def build_topic(self, message):
         json_msg = json.loads(message)
-        is_valid = {"location", "device_type", "device_id"} <= json_msg.keys()
+        
+        is_valid = REQUIRED_KEYS <= json_msg.keys()
         if not is_valid:
-            logging.error("Invalid message; missing keys")
+            logging.error(f"Cannot build topic; message is missing keys; required keys: {REQUIRED_KEYS}")
             return
         location = json_msg["location"]
         device_type = json_msg["device_type"]
