ba72caf1d1fde62d875ef1f7bbb032222ccaf0a3
Jon Shaw <jonshaw199@gmail.com>
2024-09-02 10:42:25 -0700

    fix topic builder

--- services/controller/controller.py
+++ services/controller/controller.py
@@ -8,25 +8,26 @@ from threading import Thread
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s [%(threadName)s] [%(filename)s:%(lineno)d] %(levelname)s: %(message)s',
-    handlers=[
-        logging.StreamHandler()
-    ]
+    format="%(asctime)s [%(threadName)s] [%(filename)s:%(lineno)d] %(levelname)s: %(message)s",
+    handlers=[logging.StreamHandler()],
 )
 
-# Topics: 
+# Topics:
 #   - domain_id/device_id
 #   - domain_id
 #   - <root topic>
-#   - domain_id/device_id/something/else...   <--- Can do something like this in future if necessary
-RESERVED_MSG_KEYS = ["domain_id", "device_id"]
+#   - domain_id/device_id/something/else...
+RESERVED_MSG_KEYS = ["domain", "device_id", "msg_type"]
 ROOT_TOPIC = "ROOT_TOPIC"
+
 
 class Controller:
     def __init__(self):
         self.websocket_client = WebsocketClient(self.handle_message_ws)
-        self.mqtt_client = MqttClient(self.handle_connect_mqtt, self.handle_message_mqtt)
-        
+        self.mqtt_client = MqttClient(
+            self.handle_connect_mqtt, self.handle_message_mqtt
+        )
+
     def handle_message_ws(self, message):
         logging.info("Handle message: %s", message)
         topic = self.build_topic(message)
@@ -34,7 +35,9 @@ class Controller:
             self.mqtt_client.publish(topic, message)
         else:
             # Assuming the message isn't important to the server if no topic
-            logging.error(f"Cannot publish websocket message to mqtt (no topic provided): {message}")
+            logging.error(
+                f"Cannot publish websocket message to mqtt (unable to determine topic): {message}"
+            )
 
     def handle_connect_mqtt(self):
         # Subscribe to everything; our job is to keep the server in the loop
@@ -48,20 +51,15 @@ class Controller:
 
     def build_topic(self, message):
         json_msg = json.loads(message)
-        arr = []
-        for k in RESERVED_MSG_KEYS:
-          if k in message and message[k]:
-            arr.append(message[k])
-          else:
-            break
-        return '/'.join(arr) if arr else ROOT_TOPIC
+        vals = [json_msg[key] for key in RESERVED_MSG_KEYS if key in json_msg]
+        return "/".join(vals) if vals else ROOT_TOPIC
 
     def start(self):
         mqtt_thread = Thread(target=self.mqtt_client.start)
-        mqtt_thread.name = 'MQTT Client Thread'
+        mqtt_thread.name = "MQTT Client Thread"
         mqtt_thread.start()
         self.websocket_client.start()
 
+
 if __name__ == "__main__":
     Controller().start()
-
