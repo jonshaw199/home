28a60f551d19c9784e0154d4273237e1f3c00aea
Jon Shaw <jonshaw199@gmail.com>
2024-09-11 14:51:49 -0700

    add dispatch example

--- client/components/home/Home.tsx
+++ client/components/home/Home.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import { useAppDispatch } from "@/hooks/redux";
+import { deviceSliceActions } from "@/slices/deviceSlice";
+import React, { useEffect } from "react";
 import { Platform, StatusBar, StyleSheet, Text } from "react-native";
 
 const Home = () => {
   const style = styles();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(deviceSliceActions.fetchAll());
+  }, []);
 
   return <Text style={style.container}>Home</Text>;
 };
