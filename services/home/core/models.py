a9d7e6a05238ffaf9a79739128fc75ee96611f8d
Jon Shaw <jonshaw199@gmail.com>
2024-09-11 15:48:05 -0700

    add Profile model

--- services/home/core/models.py
+++ services/home/core/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -14,3 +15,11 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    locations = models.ManyToManyField(Location, related_name="users")
+
+    def __str__(self):
+        return self.user.username
