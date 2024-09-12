f94ff7ec754e8167750d48db5475c3f05043063d
Jon Shaw <jonshaw199@gmail.com>
2024-09-11 17:33:38 -0700

    bug: install profiles after locations (and users)

--- services/home/entrypoint.sh
+++ services/home/entrypoint.sh
@@ -2,8 +2,8 @@
 
 python manage.py migrate
 python manage.py loaddata core/fixtures/users.json
-python manage.py loaddata core/fixtures/profiles.json
 python manage.py loaddata core/fixtures/locations.json
+python manage.py loaddata core/fixtures/profiles.json
 python manage.py loaddata devices/fixtures/device_types.json
 python manage.py loaddata devices/fixtures/devices.json
 python manage.py runserver 0.0.0.0:8000
