b7b49f43397783343c468e52c34a3528ea8d7a4b
Jon Shaw <jonshaw199@gmail.com>
2024-09-12 20:47:06 -0700

    fix reconnection bug

--- services/controller/websocket_client.py
+++ services/controller/websocket_client.py
@@ -8,20 +8,23 @@ import time
 from collections import deque
 
 load_dotenv()
-HOME_HOST = os.getenv('HOME_HOST')
-HOME_PORT = os.getenv('HOME_PORT')
+HOME_HOST = os.getenv("HOME_HOST")
+HOME_PORT = os.getenv("HOME_PORT")
+
 
 class WebsocketClient:
     def __init__(self, message_handler):
         self.message_handler = message_handler
         self.message_queue = deque()  # Queue to store unsent messages
+        self.ws = None
+        self.is_reconnecting = False
         self.connect()
 
     def connect(self):
         token = Auth().get_token()
         websocket.enableTrace(True)
         self.ws = websocket.WebSocketApp(
-            f'ws://{HOME_HOST}:{HOME_PORT}/ws/controllers?token={token}',
+            f"ws://{HOME_HOST}:{HOME_PORT}/ws/controllers?token={token}",
             on_open=self.on_open,
             on_message=self.on_message,
             on_error=self.on_error,
@@ -32,13 +35,27 @@ class WebsocketClient:
         self.message_handler(message)
 
     def on_error(self, ws, error):
-        logging.error(error)
+        logging.error(f"WebSocket error: {error}")
 
     def on_close(self, ws, close_status_code, close_msg):
-        logging.info(f"### Connection closed (status: {close_status_code}, message: {close_msg}). Reconnecting in 5 seconds... ###")
-        time.sleep(5)
-        self.connect()
-        self.ws.run_forever(dispatcher=rel)
+        logging.info(
+            f"### Connection closed (status: {close_status_code}, message: {close_msg}). Reconnecting... ###"
+        )
+        self.reconnect()
+
+    def reconnect(self):
+        if not self.is_reconnecting:
+            self.is_reconnecting = True
+            while True:
+                try:
+                    logging.info("Attempting to reconnect...")
+                    self.connect()
+                    self.ws.run_forever(dispatcher=rel)
+                    break  # Break loop if reconnection succeeds
+                except Exception as e:
+                    logging.error(f"Reconnection failed: {e}. Retrying in 5 seconds...")
+                    time.sleep(5)
+            self.is_reconnecting = False
 
     def on_open(self, ws):
         logging.info("Opened connection")
@@ -61,11 +78,7 @@ class WebsocketClient:
 
     def start(self):
         self.ws.run_forever(
-            dispatcher=rel,
-            ping_interval=30,
-            ping_timeout=10,
-            ping_payload="keepalive"
+            dispatcher=rel, ping_interval=30, ping_timeout=10, ping_payload="keepalive"
         )
         rel.signal(2, rel.abort)  # Handle Keyboard Interrupt (Ctrl+C)
         rel.dispatch()
-
