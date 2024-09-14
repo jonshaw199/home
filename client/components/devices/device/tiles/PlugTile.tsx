e5abfbcc3a2a19c661a3ad8eb955e07be55919d0
Jon Shaw <jonshaw199@gmail.com>
2024-09-14 16:03:12 -0700

    send WS message onPress

--- client/components/devices/device/tiles/PlugTile.tsx
+++ client/components/devices/device/tiles/PlugTile.tsx
@@ -1,10 +1,33 @@
+import WebSocketManager from "@/ws/WebSocketManager";
 import BaseTile, { BaseTileProps } from "./BaseTile";
+import { useAppSelector } from "@/store";
+import { useMemo } from "react";
+
+const HANDLER_SHELLY_PLUG = "shellyplug__set";
 
 export type PlugTileProps = Pick<BaseTileProps, "device">;
 
-export default function PlugTile(props: PlugTileProps) {
+export default function PlugTile({ device }: PlugTileProps) {
+  const plugs = useAppSelector((state) => state.plugs.data);
+
+  //const plug = useMemo(() => plugs[device.], [plugs])
+
   const handlePress = () => {
-    console.log("test");
+    const ws = WebSocketManager.getInstance();
+    const msg = {
+      action: HANDLER_SHELLY_PLUG,
+      body: {
+        device_id: device.id,
+        is_on: true,
+      },
+    };
+
+    try {
+      //ws.sendMessage(JSON.stringify(msg));
+    } catch (e) {
+      console.error("Error when sending Plug message", e);
+    }
   };
-  return <BaseTile {...props} pressableProps={{ onPress: handlePress }} />;
+
+  return <BaseTile device={device} pressableProps={{ onPress: handlePress }} />;
 }
