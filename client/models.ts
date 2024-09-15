ee533d206bc4d3424d2d8a4df581d6807f9fdce1
Jon Shaw <jonshaw199@gmail.com>
2024-09-14 18:23:20 -0700

    bug: fix incorrect UUIDs

--- client/models.ts
+++ client/models.ts
@@ -15,8 +15,8 @@ export interface User extends Identifiable {
 
 export enum DeviceTypes {
   DIAL = "18014960-6d98-412c-a62d-e400c647112e",
-  PC = "380d7a59-f957-41f3-9d51-def726b9e52a",
-  PLUG = "cf7d8486-18ad-4064-b757-e671a4749a3e",
+  PC = "7d69fa8c-c969-427d-836f-f45523e5dbb5",
+  PLUG = "f8773a6b-f0f0-489c-aa0a-fca087020069",
 }
 
 export interface DeviceType extends Identifiable {
