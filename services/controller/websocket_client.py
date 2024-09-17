0a5516177ea9dd4105863c9d43dc205dd0e0c00a
Jon Shaw <jonshaw199@gmail.com>
2024-09-17 13:49:33 -0700

    another reconnect fix

--- services/controller/websocket_client.py
+++ services/controller/websocket_client.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from auth import Auth
 import logging
 import time
-from collections import deque
 
 load_dotenv()
 HOME_HOST = os.getenv("HOME_HOST")
@@ -19,15 +18,24 @@ class WebsocketClient:
         self.connect()
 
     def connect(self):
-        token = Auth().get_token()
+        try:
+            token = Auth().get_token()
+        except Exception as e:
+            logging.error("Failed to get auth token")
+            return
+
         websocket.enableTrace(True)
-        self.ws = websocket.WebSocketApp(
-            f"ws://{HOME_HOST}:{HOME_PORT}/ws/controllers?token={token}",
-            on_open=self.on_open,
-            on_message=self.on_message,
-            on_error=self.on_error,
-            on_close=self.on_close,
-        )
+
+        try:
+            self.ws = websocket.WebSocketApp(
+                f"ws://{HOME_HOST}:{HOME_PORT}/ws/controllers?token={token}",
+                on_open=self.on_open,
+                on_message=self.on_message,
+                on_error=self.on_error,
+                on_close=self.on_close,
+            )
+        except Exception as e:
+            logging.error("Failed to connect to websocket server")
 
     def on_message(self, ws, message):
         self.message_handler(message)
@@ -39,28 +47,39 @@ class WebsocketClient:
         logging.info(
             f"### Connection closed (status: {close_status_code}, message: {close_msg}). ###"
         )
+        self.ws = None
 
     def on_open(self, ws):
         logging.info("Opened connection")
 
     def send(self, message):
         logging.info(f"Sending websocket message: {message}")
-        try:
-            self.ws.send(message)
-        except Exception as e:
-            logging.error(f"Send failed: {e}")
-            self.ws.close()
+        if self.ws:
+            try:
+                self.ws.send(message)
+            except Exception as e:
+                logging.error(f"Send failed: {e}")
+                self.ws.close()
+                self.ws = None
+        else:
+            logging.warn("Unable to send; not connected")
 
     def start(self):
         while 1:
-            try:
-                self.ws.run_forever(
-                    dispatcher=rel,
-                    ping_interval=30,
-                    ping_timeout=10,
-                    ping_payload="keepalive",
-                    # https://github.com/websocket-client/websocket-client/issues/863#issuecomment-1261353428
-                    reconnect=5,
-                )
-            except:
-                pass
+            while self.ws:
+                try:
+                    self.ws.run_forever(
+                        dispatcher=rel,
+                        ping_interval=30,
+                        ping_timeout=10,
+                        ping_payload="keepalive",
+                        # https://github.com/websocket-client/websocket-client/issues/863#issuecomment-1261353428
+                        reconnect=5,
+                    )
+                except:
+                    pass
+
+            logging.warn("Attempting to reconnect...")
+            self.connect()
+            if not self.ws:
+                time.sleep(5)
