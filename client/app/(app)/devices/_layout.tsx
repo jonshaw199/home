493c1f2cd8797aab53f624796b57a92fa869f804
Jon Shaw <jonshaw199@gmail.com>
2024-09-19 02:11:19 -0700

    styling

--- client/app/(app)/devices/_layout.tsx
+++ client/app/(app)/devices/_layout.tsx
@@ -1,14 +1,30 @@
 // app/devices/_layout.tsx
+import { Theme, useTheme } from "@/providers/ThemeProvider";
 import { useRouteInfo } from "expo-router/build/hooks";
 import { Stack } from "expo-router/stack";
+import { StyleSheet } from "react-native";
 
 export default function DevicesLayout() {
+  const theme = useTheme();
+  const style = styles(theme);
   const { segments } = useRouteInfo();
 
   return (
-    <Stack screenOptions={{ headerShown: segments.length > 2 }}>
+    <Stack
+      screenOptions={{
+        headerShown: segments.length > 2,
+        headerStyle: style.stackHeader,
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Device List" }} />
       <Stack.Screen name="[id]" options={{ title: "Device Details" }} />
     </Stack>
   );
 }
+
+const styles = (theme: Theme) =>
+  StyleSheet.create({
+    stackHeader: {
+      backgroundColor: theme.light,
+    },
+  });
