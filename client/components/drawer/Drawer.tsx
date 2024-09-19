e8df4f3987b95cfef9e43c041cf56b22bbd8773e
Jon Shaw <jonshaw199@gmail.com>
2024-09-18 20:55:59 -0700

    drawer styling

--- client/components/drawer/Drawer.tsx
+++ client/components/drawer/Drawer.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect } from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
 import {
   DrawerContentScrollView,
   DrawerItemList,
@@ -15,21 +15,73 @@ import { WS_CONNECT } from "@/ws/websocketActionTypes";
 import { environmentalSliceActions } from "@/store/slices/environmentalSlice";
 import { selectSession, signOut } from "@/store/slices/sessionSlice";
 import { useRouteInfo } from "expo-router/build/hooks";
+import { Theme, useTheme } from "@/providers/ThemeProvider";
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
+
+// Custom drawer item component
+const CustomDrawerItem: React.FC<{
+  label: string;
+  onPress: () => void;
+  active: boolean;
+  icon?: React.ReactNode;
+}> = ({ label, onPress, active, icon }) => {
+  const theme = useTheme();
+  const style = styles(theme);
+
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[style.drawerItem, active ? style.drawerItemActive : {}]}
+    >
+      {icon}
+      <Text
+        style={[style.drawerItemText, active ? style.drawerItemActiveText : {}]}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const navIconSize = 25;
 
 // Custom Drawer Content Component with TypeScript types
 const CustomDrawerContent: React.FC<DrawerContentComponentProps> = (props) => {
   const dispatch = useAppDispatch();
+  const theme = useTheme();
+  const style = styles(theme);
 
   return (
     <DrawerContentScrollView
       {...props}
-      contentContainerStyle={styles.drawerContainer}
+      contentContainerStyle={style.drawerContainer}
     >
-      <View style={styles.navItemsContainer}>
-        <DrawerItemList {...props} />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button title="Log Out" onPress={() => dispatch(signOut())} />
+      <View style={style.navItemsContainer}>
+        {Object.values(props.descriptors).map(({ options, route }, index) => (
+          <CustomDrawerItem
+            key={index}
+            label={options.title || route.name}
+            onPress={() => props.navigation.navigate(route.name)}
+            active={props.state.index === index}
+            icon={
+              options.drawerIcon &&
+              options.drawerIcon({
+                color: theme.white,
+                focused: props.state.index === index,
+                size: navIconSize,
+              })
+            }
+          />
+        ))}
+        <CustomDrawerItem
+          key="drawer_item__log_out"
+          label="Log Out"
+          onPress={() => dispatch(signOut())}
+          active={false}
+          icon={
+            <Feather name="log-out" color={theme.white} size={navIconSize} />
+          }
+        />
       </View>
     </DrawerContentScrollView>
   );
@@ -71,23 +123,64 @@ export default function Drawer() {
         screenOptions={{ headerShown: segments.length < 3 }}
         drawerContent={(props) => <CustomDrawerContent {...props} />}
       >
-        <ExpoDrawer.Screen name="index" options={{ title: "Home" }} />
-        <ExpoDrawer.Screen name="devices" options={{ title: "Devices" }} />
+        <ExpoDrawer.Screen
+          name="index"
+          options={{
+            title: "Home",
+            drawerIcon: ({ color, size }) => (
+              <Feather name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <ExpoDrawer.Screen
+          name="devices"
+          options={{
+            title: "Devices",
+            drawerIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="devices"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
       </ExpoDrawer>
     </GestureHandlerRootView>
   );
 }
 
 // Styles
-const styles = StyleSheet.create({
-  drawerContainer: {
-    flex: 1,
-    justifyContent: "space-between",
-  },
-  navItemsContainer: {},
-  buttonContainer: {
-    padding: 10,
-    borderTopWidth: 1,
-    borderTopColor: "#ccc",
-  },
-});
+const styles = (theme: Theme) =>
+  StyleSheet.create({
+    drawerContainer: {
+      flex: 1,
+      justifyContent: "space-between",
+      backgroundColor: theme.black,
+    },
+    buttonContainer: {
+      padding: 10,
+    },
+    navItemsContainer: {
+      flex: 1,
+    },
+    drawerItem: {
+      paddingVertical: 16,
+      paddingHorizontal: 16,
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 15,
+    },
+    drawerItemText: {
+      color: theme.white, // Default text color
+      fontSize: 18,
+      marginTop: 4,
+    },
+    drawerItemActive: {
+      backgroundColor: theme.primary, // Active item background color
+    },
+    drawerItemActiveText: {
+      color: theme.white, // Active item text color
+    },
+  });
