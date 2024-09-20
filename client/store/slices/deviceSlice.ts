7b2bb3a327c7f1d0d012f99e7c4462447e149afe
Jon Shaw <jonshaw199@gmail.com>
2024-09-19 23:00:51 -0700

    keep lastStatusUpdate up to date

--- client/store/slices/deviceSlice.ts
+++ client/store/slices/deviceSlice.ts
@@ -1,7 +1,34 @@
-import { Device } from "@/models";
+import { Action, Device } from "@/models";
 import { createModelSlice } from "./createModelSlice";
+import { websocketMsgReceivedAction } from "@/ws/websocketActions";
 
-const deviceSlice = createModelSlice<Device>("devices", "devices");
+const deviceSlice = createModelSlice<Device>(
+  "devices",
+  "devices",
+  (builder) => {
+    builder.addCase(websocketMsgReceivedAction, (state, { payload }) => {
+      // If this is a `system__status` message, update state
+      try {
+        const json = JSON.parse(payload || "");
+        if ("src" in json && json.src in state.data) {
+          console.info("Updating state for device ID:", json.src);
+          return {
+            ...state,
+            data: {
+              ...state.data,
+              [json.src]: {
+                ...state.data[json.src],
+                lastStatusUpdate: Date(),
+              },
+            },
+          };
+        }
+      } catch (e) {}
+
+      return state;
+    });
+  }
+);
 
 export const deviceSliceReducer = deviceSlice.reducer;
 export const deviceSliceActions = deviceSlice.actions;
