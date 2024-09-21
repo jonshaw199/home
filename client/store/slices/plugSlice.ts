6835d8c37a0294272df26a3e07c7a67e0c301c4b
Jon Shaw <jonshaw199@gmail.com>
2024-09-21 00:54:40 -0700

    handle plug websocket messages

--- client/store/slices/plugSlice.ts
+++ client/store/slices/plugSlice.ts
@@ -1,7 +1,35 @@
-import { Plug } from "@/models";
+import { Action, Plug } from "@/models";
 import { createModelSlice } from "./createModelSlice";
+import { websocketMsgReceivedAction } from "@/ws/websocketActions";
 
-const plugSlice = createModelSlice<Plug>("plugs", "plugs");
+const plugSlice = createModelSlice<Plug>("plugs", "plugs", (builder) => {
+  builder.addCase(websocketMsgReceivedAction, (state, { payload }) => {
+    // If this is a `plug__status` message, update state
+    try {
+      const json = JSON.parse(payload || "");
+      if (json.action === Action.STATUS_PLUG) {
+        const plug = Object.values(state.data).find(
+          ({ device }) => device === json.src
+        );
+        if (plug) {
+          console.info("Updating state for plug ID:", plug.id);
+          return {
+            ...state,
+            data: {
+              ...state.data,
+              [plug.id]: {
+                ...state.data[plug.id],
+                isOn: json.body.is_on,
+              },
+            },
+          };
+        }
+      }
+    } catch (e) {}
+
+    return state;
+  });
+});
 
 export const plugSliceReducer = plugSlice.reducer;
 export const plugSliceActions = plugSlice.actions;
