8daef1c4611c8aa663981dfe9bc826119903184a
Jon Shaw <jonshaw199@gmail.com>
2024-09-23 12:39:16 -0700

    reconnect woes; get rid of recursive nonsense

--- services/controller/websocket_client.py
+++ services/controller/websocket_client.py
@@ -18,48 +18,40 @@ class WebsocketClient:
         self.websocket = None
 
     async def connect(self, token=""):
+        while 1:
+            try:
+                await self._connect(token)
+            except Exception as e:
+                logging.error(f"Websocket connect error: {e}")
+                await asyncio.sleep(1)
+                logging.warn("Attempting to reconnect to WebSocket server...")
+
+    async def _connect(self, token):
         if token:
             self.token = token
         uri = f"ws://{HOME_HOST}:{HOME_PORT}/ws/controllers?token={self.token}"
         headers = {
             "Origin": f"http://{HOME_HOST}:{HOME_PORT}",
         }
+
         logging.info(f"Connecting to uri: {uri}")
 
-        try:
-            self.websocket = await websockets.connect(uri, extra_headers=headers)
-            logging.info(f"Connected to WebSocket server: {uri}")
-            await self.receive_messages()  # Start receiving messages
-        except Exception as e:
-            logging.error(f"Connection failed: {e}")
-            await self.reconnect()
-
-    async def reconnect(self):
-        logging.info("Attempting to reconnect to WebSocket server...")
-        await asyncio.sleep(5)  # Wait before reconnecting
-        await self.connect()
+        self.websocket = await websockets.connect(uri, extra_headers=headers)
+        logging.info(f"Connected to WebSocket server: {uri}")
+        await self.receive_messages()  # Start receiving messages
 
     async def receive_messages(self):
         try:
             async for message in self.websocket:
                 logging.info(f"Received message: {message}")
                 self.on_message(message)  # Call the provided callback
-        except websockets.ConnectionClosed as e:
-            logging.warning(f"Connection closed: {e}")
-            await self.reconnect()
         except Exception as e:
             logging.error(f"Error receiving message: {e}")
-            await self.reconnect()
+            raise e
 
     async def send(self, message):
+        logging.info(f"Sending websocket message: {message}")
         try:
-            if self.websocket and self.websocket.open:
-                await self.websocket.send(message)
-                logging.info(f"Sent message: {message}")
-            else:
-                logging.warning("WebSocket connection is not open. Reconnecting...")
-                await self.reconnect()
-                await self.websocket.send(message)
+            await self.websocket.send(message)
         except Exception as e:
-            logging.error(f"Error sending message: {e}")
-            await self.reconnect()
+            logging.error(f"Error sending websocket message: {e}")
