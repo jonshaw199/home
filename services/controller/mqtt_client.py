e376b3fbd3878865d95d242ee3cee5c75adfba2a
Jon Shaw <jonshaw199@gmail.com>
2024-09-23 11:09:10 -0700

    share client instance for pub and sub; fix noLocal option (infinite msg  loop)

--- services/controller/mqtt_client.py
+++ services/controller/mqtt_client.py
@@ -1,13 +1,14 @@
 import os
 import asyncio
 from paho.mqtt.subscribeoptions import SubscribeOptions
-from aiomqtt import Client, MqttError
+from aiomqtt import Client, MqttError, ProtocolVersion
 from dotenv import load_dotenv
 import logging
 
 load_dotenv()
 MQTT_BROKER_HOST = os.getenv("MQTT_BROKER_HOST")
 MQTT_BROKER_PORT = int(os.getenv("MQTT_BROKER_PORT", 1883))
+DEVICE_ID = os.getenv("DEVICE_ID", "controller")
 
 url = f"{MQTT_BROKER_HOST}/{MQTT_BROKER_PORT}"
 
@@ -15,6 +16,12 @@ url = f"{MQTT_BROKER_HOST}/{MQTT_BROKER_PORT}"
 class AsyncMqttClient:
     def __init__(self, on_message):
         self.on_message = on_message
+        self.client = Client(
+            MQTT_BROKER_HOST,
+            port=MQTT_BROKER_PORT,
+            protocol=ProtocolVersion.V5,
+            identifier=DEVICE_ID,
+        )
 
     async def handle_message(self, message):
         logging.info(f"Received message on topic {message.topic}")
@@ -34,13 +41,15 @@ class AsyncMqttClient:
 
     async def publish(self, topic, message):
         logging.info(f"Publishing message to topic {topic}: {message}")
-        async with Client(MQTT_BROKER_HOST, port=MQTT_BROKER_PORT) as client:
-            await client.publish(topic, message)
+        await self.client.publish(topic, message)
 
     async def subscribe(self, topic):
         logging.info(f"Subscribing to topic: {topic}")
-        async with Client(MQTT_BROKER_HOST, port=MQTT_BROKER_PORT) as client:
-            options = SubscribeOptions(noLocal=True)
-            await client.subscribe(topic, options=options)
-            async for message in client.messages:
-                await self.handle_message(message)
+        try:
+            async with self.client as client:
+                options = SubscribeOptions(noLocal=True)
+                await client.subscribe(topic, options=options)
+                async for message in client.messages:
+                    await self.handle_message(message)
+        except Exception as e:
+            logging.error(f"Subscribe error: {e}")
